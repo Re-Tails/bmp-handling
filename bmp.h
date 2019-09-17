cc7b0c06fbaee163a80083d523f8a97203771fbc
Tien Long Lam <lamtienlong9@gmail.com>
2019-09-17 15:14:32 +1000

    Update bmp.h
    cleaned up the URLs

--- bmp.h
+++ bmp.h
@@ -7,11 +7,10 @@
 /**
  * Common Data Types 
  *
- * The data types in this section are essentially aliases for C/C++ 
+ * Data types in this section are aliases for C/C++ 
  * primitive data types.
  *
- * Adapted from https://msdn.microsoft.com/en-us/library/cc230309.aspx.
- * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
+ * Adapted from https://msdn.microsoft.com/en-us/library/cc230309
  */
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
@@ -24,7 +23,7 @@ typedef uint16_t WORD;
  * The BITMAPFILEHEADER structure contains information about the type, size,
  * and layout of a file that contains a DIB [device-independent bitmap].
  *
- * Adapted from https://msdn.microsoft.com/en-us/library/dd183374(v=vs.85).aspx.
+ * Adapted from https://msdn.microsoft.com/en-us/library/dd183374
  */
 typedef struct 
 { 
@@ -42,7 +41,7 @@ BITMAPFILEHEADER;
  * The BITMAPINFOHEADER structure contains information about the 
  * dimensions and color format of a DIB [device-independent bitmap].
  *
- * Adapted from https://msdn.microsoft.com/en-us/library/dd183376(v=vs.85).aspx.
+ * Adapted from https://msdn.microsoft.com/en-us/library/dd183376
  */
 typedef struct
 {
@@ -63,10 +62,10 @@ BITMAPINFOHEADER;
 /**
  * RGBTRIPLE
  *
- * This structure describes a color consisting of relative intensities of
- * red, green, and blue.
+ * The RGBTRIPLE structure describes a color consisting of 
+ * relative intensities of red, green, and blue. 
  *
- * Adapted from https://msdn.microsoft.com/en-us/library/dd162939(v=vs.85).aspx.
+ * Adapted from https://msdn.microsoft.com/en-us/library/dd162939
  */
 typedef struct
 {
