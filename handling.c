2c8f75dc54dc145eb2bc48dc8c8793c962a6c444
Noa Pruth <noa.pruth@gmail.com>
2019-09-18 13:59:14 +1000

    nothing major

--- handling.c
+++ handling.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 BMPIMAGE LoadBMP(char* filename);
-void SaveBMP(char* filename, BMPIMAGE* bitmapImage);
+void SaveBMP(char* filename, BMPIMAGE bitmapImage);
 
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 	if (1 == 1)
 	{
 		BMPIMAGE bitmapImage;
-		bitmapImage = LoadBMP("LAND.BMP");
+		bitmapImage = LoadBMP("small_pic.bmp");
 		printf("%d\n", bitmapImage.bitmapInfoHeader.biSize);
 		printf("%d\n", bitmapImage.bitmapInfoHeader.biHeight);
 		printf("%d\n", bitmapImage.bitmapInfoHeader.biWidth);
@@ -22,8 +22,9 @@ int main(void)
 		printf("%d\n", bitmapImage.bitmapInfoHeader.biBitCount);
 		printf("%d\n", bitmapImage.bitmapInfoHeader.biCompression);
 		printf("%d\n", bitmapImage.bitmapInfoHeader.biSizeImage);
+		printf("%s", bitmapImage.image);
 
-		SaveBMP("SP2.BMP", &bitmapImage);
+		SaveBMP("SPY.BMP", bitmapImage);
 	}
 	
 	return 0;
@@ -45,7 +46,7 @@ int main(void)
 BMPIMAGE LoadBMP(char* filename)
 {
 	FILE *BMP_p;
-	BMPIMAGE *bitmapImage = malloc(sizeof(*bitmapImage));
+	BMPIMAGE bitmapImage;
 	/*The actual picture*/
 
 	BMP_p = fopen(filename, "rb");
@@ -54,34 +55,31 @@ BMPIMAGE LoadBMP(char* filename)
 	{
 		printf("File does not exist\n");
 	}
-	int elements_read;
-
-	elements_read = fread(&bitmapImage->bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, BMP_p);
-	printf("%d\n", elements_read);
-	elements_read = fread(&bitmapImage->bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, BMP_p);
-	printf("%d\n", elements_read);
+	
+	fread(&bitmapImage.bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, BMP_p);
+	fread(&bitmapImage.bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, BMP_p);
 	/*Move to beginning of bits*/
-	/*fseek(BMP_p, bitmapImage->bitmapFileHeader.bfOffBits, SEEK_SET);*/
+	/*fseek(BMP_p, bitmapImage->bitmapFileHeader.bfOffBits, SEEK_SET); Does not work? */
 	/*Allocate memory for the image*/
-	printf("SIZE: %d\n", bitmapImage->bitmapInfoHeader.biSizeImage);
-	bitmapImage->image = (unsigned char*) malloc(bitmapImage->bitmapInfoHeader.biSizeImage);
+	printf("SIZE: %d\n", bitmapImage.bitmapInfoHeader.biSizeImage);
+	bitmapImage.image = (unsigned char*) malloc(bitmapImage.bitmapInfoHeader.biSizeImage);
 	printf("test");
-	fread(bitmapImage->image, bitmapImage->bitmapInfoHeader.biSizeImage, 1, BMP_p);
+	fread(bitmapImage.image, bitmapImage.bitmapInfoHeader.biSizeImage, 1, BMP_p);
 	fclose(BMP_p);
-	return *bitmapImage;
+	return bitmapImage;
 }
 
 
 
-void SaveBMP(char* filename, BMPIMAGE* bitmapImage)
+void SaveBMP(char* filename, BMPIMAGE bitmapImage)
 {
 	FILE *BMP_p;
 
 	BMP_p = fopen(filename, "wb");
 
-	fwrite(&bitmapImage->bitmapFileHeader, sizeof(bitmapImage->bitmapFileHeader), 1, BMP_p);
-	fwrite(&bitmapImage->bitmapInfoHeader, sizeof(bitmapImage->bitmapInfoHeader), 1, BMP_p);
-	fwrite(bitmapImage->image, bitmapImage->bitmapInfoHeader.biSizeImage, 1, BMP_p);
+	fwrite(&bitmapImage.bitmapFileHeader, sizeof(bitmapImage.bitmapFileHeader), 1, BMP_p);
+	fwrite(&bitmapImage.bitmapInfoHeader, sizeof(bitmapImage.bitmapInfoHeader), 1, BMP_p);
+	fwrite(bitmapImage.image, bitmapImage.bitmapInfoHeader.biSizeImage, 1, BMP_p);
 	
 	fclose(BMP_p);
 }
