182a0be2697b4b17a911ebca719430bc01c16411
bulltrollape <55420849+bulltrollape@users.noreply.github.com>
2019-09-18 17:48:00 +1000

    Add files via upload
    a set of prototypes being submitted with the scope

---
+++ prototype.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void password ();
+
+void image ();
+
+void compress(char* image);
+
+void encrypt(int NumPass ,char* image);
+
+void decrypt(int NumPass ,char* image);
+
+void decompress(char* image);
+
+int main {
+	/*print menu then declare functions based on user input */
+
+};
+
+void password (){
+
+/*collect string from user make into numbers make 50 lenght through repetiotion
+of password */
+};
+
+void image (){
+
+/* open image and create a local coppy that shall go through compression or 
+encryption */
+};
+
+void compress(char* image){
+
+/* take image from image and compress using a compressiojn algorithm*/
+};
+
+void encrypt(int NumPass ,char* image){
+/* take image and pasword to encrypt using num password as the main factor 
+in the encryption */
+
+};
+
+void decrypt(int NumPass ,char* image){
+
+	/* take image and password and decrypt using num password */ 
+
+};
+
+void decompress(char* image){
+
+	/* take the image from image and decompress using the same algorithm as compress*/
+
+};
